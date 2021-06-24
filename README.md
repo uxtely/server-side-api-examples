0a85f2457cd4043f11539fa6fc28f45caacba090
Eric Fortis <efortis@gmail.com>
2021-06-24 01:43:26 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Server-Side Card API examples for UI&nbsp;Drafter
+In UI Drafter Cards and Buttons can make HTTP requests. This repository
+has the server-side code for the examples of the documentation page.
+
+
+## Documentation and Examples
+https://docs.uidrafter.com/code#-examples-with-http-requests
+
+
+## Install
+```shell script
+npm install
+```
+Uses Node + Express.
+
+
+## Run
+```shell script
+node examples.js
+```
+It runs on http://localhost:7000
+
+
+## Disclaimer
+The examples are for illustration purposes only, security and error
+handling are omitted. They're minimal, so you can quickly see what's
+particular to an UI Drafter integration.
+
---
+++ examples.js
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) Uxtely LLC. All rights reserved.
+ * Licensed under the ISC license found in the LICENSE
+ * file in the root directory of this source tree.
+ */
+
+process.on('unhandledRejection', error => { throw error });
+
+const app = require('express')();
+const jsonParser = require('body-parser').json();
+
+
+app.use((_, response, next) => {
+	response.header('Access-Control-Allow-Origin', 'https://my.uidrafter.com'); // or change it to https://free.uidrafter.com
+	response.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+});
+
+
+app.get('/in-stock-count/:productId', (request, response) => {
+	switch (request.params.productId) {
+		case 'abc':
+			response.json({ count: 200 });
+			break;
+		case 'cba':
+			response.json({ count: 2 });
+			break;
+		default:
+			response.sendStatus(404);
+	}
+});
+
+
+app.post('/hypotenuse', jsonParser, (request, response) => {
+	const card = request.body;
+	const apiKey = card.api_key.value;
+	const sideA = card.side_a.value;
+	const sideB = card.side_b.value;
+
+	if (apiKey !== 'MyApiKey')
+		response.sendStatus(401);
+	else
+		response.json({
+			hypotenuse: Math.sqrt(sideA ** 2 + sideB ** 2)
+		});
+});
+
+
+app.post('/form-submit', jsonParser, (request, response) => {
+	const card = request.body;
+	const first_name = card.first_name.value;
+	const last_name = card.last_name.value;
+
+	console.log(first_name, last_name);
+	response.sendStatus(200);
+});
+
+
+app.listen(7000, 'localhost', error => {
+	if (error)
+		console.error(error);
+	else
+		console.log('Listening on http://localhost:7000');
+});
+

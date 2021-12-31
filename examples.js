c84144a361a58eafb8a4dd3e1ebbdfeef0b6d4fd
Eric Fortis <efortis@gmail.com>
2021-12-30 22:32:16 -0400

    Rename to Uxtely

--- examples.js
+++ examples.js
@@ -11,7 +11,7 @@ const jsonParser = require('body-parser').json();
 
 
 app.use((_, response, next) => {
-	response.header('Access-Control-Allow-Origin', 'https://free.formrig.com');
+	response.header('Access-Control-Allow-Origin', 'https://free.uxtely.com');
 	response.header('Access-Control-Allow-Headers', 'Content-Type');
 	next();
 });
